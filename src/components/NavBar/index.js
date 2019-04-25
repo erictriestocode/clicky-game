fd381d3c56226b1772dbea060d675382f7b76c8e
Eric <eric.northrop@gmail.com>
2019-04-24 21:49:49 -0500

    Navbar Assembly and CSS import/create

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,6 +1,18 @@
 import React from "react";
+import "./style.css";
 // TODO: Add Styles
 
-function NavBar(props){
-    return
+function NavBar(props) {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li className="title">
+                    <a href="/">Star Wars Clicky Game!</a>
+                </li>
+                <li className="status">The force is strong in you!</li>
+            </ul>
+        </nav>
+    )
 }
+
+export default NavBar;
